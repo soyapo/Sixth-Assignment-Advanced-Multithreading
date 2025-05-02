c52289836f85a8b464260f4f36ce74ba9c0631d7
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-02 19:38:58 +0330

    feat: add main method in Assignment01 and Assignment02

--- src/main/java/Assignment01.java
+++ src/main/java/Assignment01.java
@@ -1,2 +1,6 @@
 public class Assignment01 {
+    public static void main(String[] args)
+    {
+        
+    }
 }
--- src/main/java/Assignment02.java
+++ src/main/java/Assignment02.java
@@ -1,2 +1,6 @@
 public class Assignment02 {
+    public static void main(String[] args)
+    {
+
+    }
 }
