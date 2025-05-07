b12bde732219233c46a5e61198a61780c3017df9
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-07 19:24:42 +0330

    fix: adjust TODOs

--- src/main/java/MonteCarloPI/MonteCarloPi.java
+++ src/main/java/MonteCarloPI/MonteCarloPi.java
@@ -1,22 +1,17 @@
 package MonteCarloPI;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MonteCarloPi {
 
-    static final long NUM_POINTS = 1_000_000_000L;  // 1 billion points
-
+    static final long NUM_POINTS = 50_000_000L;
     static final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
-
     public static void main(String[] args) throws InterruptedException, ExecutionException
     {
-        System.out.printf("Tasks started with %d threads",NUM_THREADS);
         // Without Threads
+        System.out.println("Single threaded calculation started: ");
         long startTime = System.nanoTime();
         double piWithoutThreads = estimatePiWithoutThreads(NUM_POINTS);
         long endTime = System.nanoTime();
@@ -24,6 +19,7 @@ public class MonteCarloPi {
         System.out.println("Time taken (single threads): " + (endTime - startTime) / 1_000_000 + " ms");
 
         // With Threads
+        System.out.println("Multi threaded calculation started: ");
         startTime = System.nanoTime();
         double piWithThreads = estimatePiWithThreads(NUM_POINTS, NUM_THREADS);
         endTime = System.nanoTime();
@@ -31,19 +27,25 @@ public class MonteCarloPi {
         System.out.println("Time taken (Multi-threaded): " + (endTime - startTime) / 1_000_000 + " ms");
     }
 
-    // Monte Carlo Pi Approximation without threads
+    // TODO: Monte Carlo Pi Approximation without threads
     public static double estimatePiWithoutThreads(long numPoints)
     {
-        //TODO
+        // TODO: Implement this method to calculate Pi using a single thread
         return 0;
     }
 
-    // Monte Carlo Pi Approximation with threads
+    // TODO : Monte Carlo Pi Approximation with threads
     public static double estimatePiWithThreads(long numPoints, int numThreads) throws InterruptedException, ExecutionException
     {
+        // TODO: Implement this method to calculate Pi using multiple threads
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-        List<Callable<Long>> tasks = new ArrayList<>();
-        //TODO
+        // HINT: You may need to create some local variables
+
+        // TODO: Divide the work into tasks for each thread
+
+        //TODO: do not remember to shutdown executor
+        //TODO: wait for executor to be fully terminated
+        //TODO: complete the calculation and return the result
         return 0;
     }
 }
