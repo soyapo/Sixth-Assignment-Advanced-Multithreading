c761e4babbd956a1596dc2298aa256aae6ffddda
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2025-05-07 17:42:42 +0330

    Complete MonteCarlo

--- src/main/java/MonteCarloPi.java
+++ src/main/java/MonteCarloPi.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class MonteCarloPi {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class MonteCarloPi {
+
+    static final long NUM_POINTS = 1_000_000_000L;  // 1 billion points
+
+    static final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException
+    {
+        System.out.printf("Tasks started with %d threads",NUM_THREADS);
+        // Without Threads
+        long startTime = System.nanoTime();
+        double piWithoutThreads = estimatePiWithoutThreads(NUM_POINTS);
+        long endTime = System.nanoTime();
+        System.out.println("Monte Carlo Pi Approximation (single thread): " + piWithoutThreads);
+        System.out.println("Time taken (single threads): " + (endTime - startTime) / 1_000_000 + " ms");
+
+        // With Threads
+        startTime = System.nanoTime();
+        double piWithThreads = estimatePiWithThreads(NUM_POINTS, NUM_THREADS);
+        endTime = System.nanoTime();
+        System.out.println("Monte Carlo Pi Approximation (Multi-threaded): " + piWithThreads);
+        System.out.println("Time taken (Multi-threaded): " + (endTime - startTime) / 1_000_000 + " ms");
+    }
+
+    // Monte Carlo Pi Approximation without threads
+    public static double estimatePiWithoutThreads(long numPoints)
+    {
+        //TODO
+    }
+
+    // Monte Carlo Pi Approximation with threads
+    public static double estimatePiWithThreads(long numPoints, int numThreads) throws InterruptedException, ExecutionException
+    {
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+        List<Callable<Long>> tasks = new ArrayList<>();
+        //TODO
+    }
 }
