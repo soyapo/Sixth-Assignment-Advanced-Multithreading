f8100c9a3c50616615d44c87d4b4c42d98d9875d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-07 14:38:31 +0330

    fix: change some minor details

---
+++ src/main/java/Banking/BankingMain.java
@@ -0,0 +1,32 @@
+package Banking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankingMain {
+    public static void main(String[] args) throws InterruptedException {
+        List<BankAccount> accounts = new ArrayList<>();
+        accounts.add(new BankAccount(1,20000));
+        accounts.add(new BankAccount(2,20000));
+        accounts.add(new BankAccount(3,20000));
+        accounts.add(new BankAccount(4,20000));
+
+        Thread[] threads = new Thread[4];
+        for(int i = 1; i <= 4; i++){
+            String fileName = i + ".txt";
+            threads[i - 1] = new Thread(new TransactionProcessor(accounts.get(i - 1), fileName, accounts));
+        }
+
+        for(Thread thread : threads){
+            thread.start();
+        }
+
+        for(Thread thread : threads){
+            thread.join();
+        }
+
+        for(BankAccount account : accounts){
+            System.out.println("Final balance of Account Number "  + account.getId() + " : " + account.getBalance());
+        }
+    }
+}
